ddc1c2026e03599d923be8b940b25477916b1991
Yiannis Bourkelis <eu_gr_yiannis@hotmail.com>
2018-01-18 22:25:11 +0200

    removed EWOULDBLOCK polling - has no effect

--- src/open-server/html_messageprocessor.cpp
+++ src/open-server/html_messageprocessor.cpp
@@ -85,12 +85,14 @@ void HTML_MessageProcessor::processMessageQueue()
 
             //ean i proigoumeni apostoli teleiwse me EWOULDBLOCK
             //kanw poll sto socket wste na min katanalwnontai cpu cycles
+            /*
             if (pending_client_sessions_req_resp_.front().ewouldblock_flag){
                 struct pollfd wait_fd[1];
                 wait_fd[0].events = POLLIN;
                 wait_fd[0].fd = pending_client_sessions_req_resp_.front().socket;
                 poll(wait_fd, 1, 5000);
             }
+            */
 
             int snd;
             size_t total_bytes = 0;
